43166aff3b3009792e03e088385e0e8fe0641a35
jjj21004 <jjj21004@jjj21004-vm.cse.uconn.edu>
2023-09-25 15:35:12 -0400

    Lab1

---
+++ j2023_09_12/src/u/TestMeTests.java
@@ -0,0 +1,57 @@
+package u;
+
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import c.TestMe;
+
+public class TestMeTests {
+
+	TestMe testMe;
+
+	@Before
+	public void setUp() throws Exception {
+		testMe  = new TestMe();
+    }
+
+	@Test
+	public void squareTest() {
+
+		double baseValue = 3.0;
+		double expected = baseValue*baseValue;
+		testMe.setValue(3.0);
+		double actual = testMe.square();
+
+		assertTrue(actual == expected);
+	}
+	@Test
+	public void cubeTest(){
+		double baseValue = 5.0;
+		double expected = baseValue*baseValue*baseValue;
+		testMe.setValue(5.0);
+		double actual = testMe.cube();
+
+		assertTrue(actual == expected);
+	}
+
+	@Test
+        public void times_two(){
+                double baseValue = 13.0;
+                double expected = baseValue*2.0;
+                testMe.setValue(13.0);
+                double actual = testMe.times_two();
+
+                assertTrue(actual == expected);
+        }
+	@Test
+        public void times_ten(){
+                double baseValue = 12.0;
+                double expected = baseValue*10;
+                testMe.setValue(12.0);
+                double actual = testMe.times_ten();
+
+                assertTrue(actual == expected);
+        }
+}
---
+++ j2023_09_21/my-app/src/main/java/com/mycompany/app/TestMe.java
@@ -0,0 +1,20 @@
+package com.mycompany.app;
+
+public class TestMe {
+	private double value = Double.NaN;
+
+	public static void main(String[] args) {
+		System.out.println("Hello world!");
+	}
+
+	public TestMe() {}
+
+	public double square() { return value*value; }
+	public double cube() { return value*value*value; }
+	public double times_two() { return value*2; }
+	public double times_ten() { return value*10; }
+	public void setValue(double v) {
+		value = v;
+	}
+
+}
